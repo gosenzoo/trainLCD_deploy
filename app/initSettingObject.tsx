3bbae7b4a5c71b77089ea95cc1e2f192e2c3076f
gosenzoo <gosenzoo15@gmail.com>
2024-10-12 20:05:03 +0900

    通常運転・環状運転の切り替え実装

--- app/initSettingObject.tsx
+++ app/initSettingObject.tsx
@@ -1,7 +1,7 @@
 import "./type"
 
 const initSettingObject: settingType = {
-    info: {settingName: "", destination: "", direction: "", trainType: "", lineLogo: "", lineColor: "", carNumber: "", place: 'left'},
+    info: {settingName: "", destination: "", direction: "", trainType: "", lineLogo: "", lineColor: "", carNumber: "", place: 'left', isLoop: false},
     stationList: [],
     lineDict: {},
     iconDict: {}
